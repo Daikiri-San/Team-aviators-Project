479a47f6e2c8f4cf20d2b45bc9385922816118a0
Nikita Holovnin <Golovninnikita@ukr.net>
2019-12-25 02:06:50 +0200

    just an API test

---
+++ src/js/1InitialHomePage.js
@@ -0,0 +1,9 @@
+const key = '259c02d1f1f516a6001436d2cce8084d';
+const baseURL = 'https://api.themoviedb.org/3/movie';
+
+fetch(`${baseURL}/550?api_key=${key}`).then(res =>
+  res
+    .json()
+    .then(data => console.log(data))
+    .catch(error => console.log(error)),
+);
