9497b9ad93e10c712718a6344d639ecc23943230
Nikita Holovnin <Golovninnikita@ukr.net>
2019-12-27 01:51:38 +0200

    updated searchFetch obj

--- src/components/services/fetchSearchMovies.js
+++ src/components/services/fetchSearchMovies.js
@@ -2,9 +2,12 @@ const key = '259c02d1f1f516a6001436d2cce8084d';
 const baseURL = 'https://api.themoviedb.org/3/search/movie';
 
 const searchFetch = {
+  query: '',
   page: 1,
-  fetchSearchMovies(query) {
-    return fetch(`${baseURL}?api_key=${key}&page=${this.page}&query=${query}`)
+  fetchSearchMovies() {
+    return fetch(
+      `${baseURL}?api_key=${key}&page=${this.page}&query=${this.query}`,
+    )
       .then(res => res.json())
       .catch(console.log);
   },
