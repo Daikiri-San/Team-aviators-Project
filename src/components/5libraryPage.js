76753a3f406e414e014f45031c6387dc5990ff40
Nikita Holovnin <Golovninnikita@ukr.net>
2019-12-27 23:18:47 +0200

    localStorage watched add

--- src/components/5libraryPage.js
+++ src/components/5libraryPage.js
@@ -1,5 +1,7 @@
 import myLibPageMarkup from '../templates/myLibPageMarkup';
 import refs from './utils/refs';
+import filmListMarkup from '..//templates/filmListMarkup.hbs';
+import drawMovieDetails from './4filmDetailsPage';
 
 function makeLibPage() {
   refs.homePage.innerHTML = '';
@@ -11,8 +13,17 @@ function makeLibPage() {
   refs.queueButton = document.querySelector('#queue-button');
   refs.myLibHome = document.querySelector('#mylib-home');
 
-  const fromLocalStorage = JSON.parse(localStorage.getItem('film'));
-  console.log(fromLocalStorage);
+  makeListOfWatched();
+
+  refs.myLibHome.addEventListener('click', drawMovieDetails);
+}
+
+function makeListOfWatched() {
+  const fromLocalStorage = JSON.parse(localStorage.getItem('watched'));
+  const markup = fromLocalStorage
+    .map(result => filmListMarkup(result))
+    .join('');
+  refs.myLibHome.insertAdjacentHTML('beforeend', markup);
 }
 
 export default makeLibPage;
--- src/components/localStorage/localStorage.js
+++ src/components/localStorage/localStorage.js
@@ -6,10 +6,11 @@ function setLocalStorage(e) {
     newItem.id = data.id;
     newItem.poster = `http://image.tmdb.org/t/p/w500${data.poster_path}`;
     newItem.title = data.title;
+    newItem.votes = data.vote_average;
     newItem.year = data.release_date.slice(0, 4);
-    const oldItems = JSON.parse(localStorage.getItem('film')) || [];
+    const oldItems = JSON.parse(localStorage.getItem('watched')) || [];
     oldItems.push(newItem);
-    localStorage.setItem('film', JSON.stringify(oldItems));
+    localStorage.setItem('watched', JSON.stringify(oldItems));
   });
 }
 
