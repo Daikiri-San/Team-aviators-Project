75aa2df6f8e2c793afb0366678ae6f82e393dc8f
27ugeen <27ugeen@gmail.com>
2019-12-28 10:10:55 +0200

    discard changes scrool

--- src/components/view/toTopButton.js
+++ src/components/view/toTopButton.js
@@ -1,16 +1,8 @@
 import refs from '../utils/refs';
 
 function toTop() {
-  // document.body.scrollTop = 0;
-  // document.documentElement.scrollTop = 0;
-
-  const scrollOptions = {
-    left: 0,
-    top: height,
-    behavior: 'smooth',
-  };
-
-  document.body.scrollTo(scrollOptions);
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 refs.ScrollButton.addEventListener('click', toTop);
