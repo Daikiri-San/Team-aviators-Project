9c19c6733f7bbf556ef3d166525851851ae2c274
27ugeen <27ugeen@gmail.com>
2019-12-28 10:07:10 +0200

    fixed header/footer

--- src/components/view/toTopButton.js
+++ src/components/view/toTopButton.js
@@ -1,8 +1,16 @@
 import refs from '../utils/refs';
 
 function toTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+
+  const scrollOptions = {
+    left: 0,
+    top: height,
+    behavior: 'smooth',
+  };
+
+  document.body.scrollTo(scrollOptions);
 }
 
 refs.ScrollButton.addEventListener('click', toTop);
